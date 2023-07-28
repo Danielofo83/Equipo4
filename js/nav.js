9cdbbaad11dfc128fb98b2163373e042ac8d5496
NefertariLaurel <laurel_04@outlook.com>
2023-07-28 10:20:23 -0600

    Try to make a functional button

---
+++ js/nav.js
@@ -0,0 +1,30 @@
+/* AL DAR CLICK EN IMAGEN REDIRECCIONAR A OTRO LUGAR  */ 
+/* function enterClick (){
+    let eventClick = document.getElementById("EnterFormButton");
+    eventClick.addEventListener('click',(event)=> {
+       console.log(event);
+        window.open('index.html');  
+    }); 
+};
+ */
+
+let eventClick= document.getElementById("EnterFormButton")
+eventClick.addEventListener("click",() => {
+    window.open("https://dev.to/","_blank")
+});
+
+
+
+
+
+/* FILTRO PARA BUSCAR POST EN BARRA DE BUSQUEDA  */
+
+
+
+/* LOGIN Y CREATE DAN EL FORMULARIO */ 
+
+
+
+
+
+/* FORMULARIO MANDAR INFO A STORAGE  */
