5667a3a8cc3ffa5b8a3a8a5c07f9b21a3b3fbc95
NefertariLaurel <laurel_04@outlook.com>
2023-07-29 02:50:09 -0600

    Create a new carpet with seccion Login and Nav bar, delate modules

---
+++ js/NavegationBar&Login/ButtonsNav.js
@@ -0,0 +1,39 @@
+/* REDIRECCIONAMIENTO  */ 
+
+/* funcionar dev imagen  */
+
+document.getElementById ("NavInteractionIma").addEventListener("click",()=>{
+    window.open('https://dev.to/','_self')
+    console.log("Entrando a home");
+    });
+
+/* funcionar boton Login  */
+document.getElementById ("EnterFormButtonLogin").addEventListener("click",()=>{
+window.open('https://dev.to/','_self')
+console.log("Login");
+});
+
+/* https://dev.to/
+../Views/detail.html */
+
+/* funcionar boton create account */
+document.getElementById ("ButtonCreateAcount").addEventListener("click",()=>{
+    window.open('https://dev.to/','_self')
+    console.log("Crear");
+    });
+
+
+    
+
+
+/* FILTRO PARA BUSCAR POST EN BARRA DE BUSQUEDA  */
+
+
+
+/* LOGIN Y CREATE DAN EL FORMULARIO */ 
+
+
+
+
+
+/* FORMULARIO MANDAR INFO A STORAGE  */
---
+++ js/NavegationBar&Login/createNav.js
@@ -0,0 +1,117 @@
+
+/* CREAR OBJETOS POR CADA ETIQUETA EN SECCION NAV VISUALIZACION NO LOGIADO  */
+
+const createNavbarNav=(islogged)=>{
+    let navbar= document.createElement("nav")
+    navbar.classList.add("navbar","p-0"," px-3", "bg-white"," nav__container")
+     };
+
+const createNavbarDiv1=(islogged)=>{
+    let navbar= document.createElement("div")
+     navbar.classList.add("container-xl", "p-0", "justify-content-between", "flex-nowrap")
+     };
+
+const createNavbarDiv2=(islogged)=>{
+    let navbar= document.createElement("div")
+     navbar.classList.add("d-flex")
+    };
+
+const createNavbarButton1=(islogged)=>{
+     let navbar= document.createElement("button")
+    navbar.classList.add("navbar-toggler", "mx-md-1", "me-2", "border-0", "d-md-none", "px-0")
+    };
+
+const createNavbarSpan1=(islogged)=>{
+    let navbar= document.createElement("span")
+    navbar.classList.add("navbar-toggler-icon")
+       };
+
+const createNavbarA1=(islogged)=>{
+   let navbar= document.createElement("a")
+       navbar.classList.add("navbar-brand")
+       };
+
+const createNavbarImagen1=(islogged)=>{
+    let navbar= document.createElement("img")
+     navbar.classList.add("NavInteractionIma")
+     };
+
+const createNavbarDiv3=(islogged)=>{
+    let navbar= document.createElement("div")
+     navbar.classList.add("d-none, d-md-block", "m-auto", "nav__form__container")
+    };
+
+const createNavbarForm=(islogged)=>{
+    let navbar= document.createElement("form")
+     navbar.classList.add("d-flex", "form__search")
+        };
+
+const createNavbarDiv4=(islogged)=>{
+    let navbar= document.createElement("div")
+    navbar.classList.add("input-group")
+    };
+
+const createNavbarInput=(islogged)=>{
+    let navbar= document.createElement("input")
+    navbar.classList.add("form-control", "border-end-0")
+    };
+
+const createNavbarButton2=(islogged)=>{
+    let navbar= document.createElement("button")
+    navbar.classList.add("border-0", "bg-white", "p-0", "search__button")
+    };
+
+const createNavbarSpan2=(islogged)=>{
+    let navbar= document.createElement("span")
+    navbar.classList.add("bg-white", "border-1", "border-start-0", "rounded-0", "rounded-end", "input-group-text", "search__hover")
+    };
+
+const createNavbarSvg1=(islogged)=>{
+    let navbar= document.createElement("svg")
+    navbar.classList.add("crayons-icon")
+    };
+
+const createNavbarTitle1=(islogged)=>{
+    let navbar= document.createElement("title")
+    navbar.classList.add("Title_search")
+        };
+
+const createNavbarPath1=(islogged)=>{
+    let navbar= document.createElement("path")
+    navbar.classList.add("Path1")
+    };
+
+ const createNavbaDiv5=(islogged)=>{
+    let navbar= document.createElement("div")
+    navbar.classList.add("d-flex gap-3")
+        };
+
+const createNavbaButton3=(islogged)=>{
+    let navbar= document.createElement("button")
+    navbar.classList.add("Login_enter", "btn", "btn-outline-primary", "d-none", "d-md-block", "ml-1", "border", "border-white")
+    };
+    
+const createNavbaSvg2=(islogged)=>{
+    let navbar= document.createElement("svg")
+    navbar.classList.add("Ttile_search1")
+        };
+
+const createNavbarPath2=(islogged)=>{
+    let navbar= document.createElement("path")
+     navbar.classList.add("Path2")
+     };
+
+const createNavbarButton4=(islogged)=>{
+        let navbar= document.createElement("button")
+         navbar.classList.add("Create_account","btn", "btn-outline-primary", "d-none", "d-md-block")
+         };
+
+const createNavbaSvg3=(islogged)=>{
+    let navbar= document.createElement("svg")
+    navbar.classList.add("Ttile_search2")
+                };
+
+const createNavbarPath3=(islogged)=>{
+    let navbar= document.createElement("path")
+    navbar.classList.add("Path3")
+    };
